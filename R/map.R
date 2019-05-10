a33810ffad50a0241149cffc7f539e03cba1be83
Ma Xiaowen <maxiaowen1997@163.com>
2019-05-10 14:11:24 +0800

    update map.r

--- R/map.R
+++ R/map.R
@@ -1,9 +1,8 @@
-#1.在地图上绘制交互式各省份2010年预期寿命图
+#1.在地图上绘制各省份2010年预期寿命图
 
 install.packages('leafletCN')
 library(leafletCN)
 
-#数据来源：中国统计局官网
 data = read.csv("C:/Users/lenovo/Desktop/ex_life.csv",header = TRUE)
 #data缺少香港，澳门，台湾的数据
 regionNames("china")
@@ -19,8 +18,11 @@ geojsonMap(data, "china",popup =  paste0(data$Province, ":", data$ex_life2010),
 require(leaflet)
 # %>% 把左件的值发送给右件的表达式，并作为右件表达式函数的第一个参数
 
-#带标记
-leaflet() %>% addTiles() %>% addMarkers(lat = 40.169, lng = 116.275)
 #不带标记
 leaflet() %>% addTiles() %>% setView(lat = 40.169, lng = 116.275, zoom = 14)
-
+#带标记
+leaflet() %>% addTiles() %>% addMarkers(lat = 40.169, lng = 116.275)
+#比心
+heart = data.frame(lat = 40.171 + c(0,0.0003,0.0007,0.0003,-0.0003,-0.0009,-0.0014,-0.0019,-0.0023,0.0003,0.0007,0.0003,-0.0003,-0.0009,-0.0014,-0.0019), 
+               lng = 116.2775+c(0,0.0008,0.0015,0.0021,0.0018,0.0015,0.001,0.0005,0,-0.0008,-0.0015,-0.0021,-0.0018,-0.0015,-0.001,-0.0005))
+heart %>% leaflet() %>% addTiles() %>% addMarkers()
